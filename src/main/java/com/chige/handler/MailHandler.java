74386ee3675cb238c1cf7b862121d4bcb9062bf0
xiaowangc <2440477909@qq.com>
2020-09-05 09:36:49 +0800

    发送邮件、订阅邮件实例、带模板格式的邮件

--- src/main/java/com/chige/handler/MailHandler.java
+++ src/main/java/com/chige/handler/MailHandler.java
@@ -1,7 +1,7 @@
 package com.chige.handler;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.FileSystemResource;
+
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
